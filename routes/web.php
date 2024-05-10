883a7c3a77103375b5263f16220d9c332280263e
juanfer1109 <juanfer_arango@hotmail.com>
2024-05-10 18:06:15 -0500

    Saludito

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CursoController;
 use App\Http\Controllers\EstudianteController;
 use App\Http\Controllers\welcomeController;
+use App\Http\Controllers\SaludoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [welcomeController::class, 'index']);
@@ -10,3 +11,5 @@ Route::get('/', [welcomeController::class, 'index']);
 Route::resource('/cursos', CursoController::class);
 
 Route::resource('/estudiantes', EstudianteController::class);
+
+Route::get('/saludo', [SaludoController::class, 'saludito']);
